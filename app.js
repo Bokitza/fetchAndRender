2f586cbe6b4e3f5199d31bf5b6c544ace841502e
Yoav Sagi <sagiyoav@gmail.com>
2021-01-06 01:34:49 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+fetch(
+  "https://api.airtable.com/v0/appo2m5gXXd3pNNCG/Table%201?maxRecords=3&view=Grid%20view",
+  {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer key3AHJQkA3Gkjagf",
+    },
+  }
+).then((response) => response.json());
---
+++ work.js
@@ -0,0 +1,14 @@
+// -- learn --
+// go to the dogs API @ https://dog.ceo/dog-api/breeds-list and look how the API works...
+// after you realize how stuff works, read how you can fetch MULTIPLE images here: https://dog.ceo/dog-api/documentation/random
+
+// -- mission --
+//ok cool. your job is to let the user choose from some dog breeds, and press "search".
+//when he clicks - the screen gonna render 5 cute images of that breed!
+//you can use the css formatting that already exists, or make new one...
+//but make it look nice.
+
+// -- another mission --
+// lets learn some about dogs. look how you can list all the sub-breeds
+// of the breed the user chose. render this list to the screen, before the images.
+// relavant api docs is here: https://dog.ceo/dog-api/documentation/sub-breed
