325d7a40405a7f0e016d178dfda1f980c61c6b0a
Yoav Sagi <sagiyoav@gmail.com>
2021-01-06 10:17:45 +0200

    refactor

--- app.js
+++
@@ -1,9 +0,0 @@
-const url =
-  "https://api.airtable.com/v0/appo2m5gXXd3pNNCG/Table%201?maxRecords=3&view=Grid%20view";
-
-fetch(url, {
-  method: "GET",
-  headers: {
-    Authorization: "Bearer key3AHJQkA3Gkjagf",
-  },
-}).then((response) => response.json());
